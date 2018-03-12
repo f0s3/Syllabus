4dba6a433b069a87aed0cf0f6da7dc3b8ffe341d
f0s3 <power.aerozoll@gmail.com>
2018-03-12 22:35:56 +0200

    #5 Trying to get a model working and in the variable

--- public/js/controllers/TableController.js
+++ public/js/controllers/TableController.js
@@ -1,7 +1,16 @@
 function tableController(tableMetaData) {
     let tableData = [];
     let day = "";
-    //todo: let tableInfo = require(tableInfo) somehow
+    let tableInfo;
+
+    console.log(tableMetaData);
+    fetch("http://localhost:4444/js/models/tableInfo.json")
+    .then(promise => promise.json())
+    .then(response => tableInfo = response)
+    .then(() => console.log(tableInfo));
+
+    console.log(tableInfo);
+
     for (let i = 0; i < tableMetaData.length; i++) {
         if (tableMetaData[i]['weekId'] === 1) {
             tableData.push(tableMetaData[i].date,tableInfo.monday);
@@ -12,8 +21,14 @@ function tableController(tableMetaData) {
         else if (tableMetaData[i].weekId === 4) day = "thursday";
         else if (tableMetaData[i].weekId === 5) day = "friday";
         else if (tableMetaData[i].weekId === 6 || tableMetaData[i].weekId === 7) day = "";
-        console.log(day);
+        //console.log(day);
     }
+
+    /*
+    tableData = [
+     {tableMetaData: ... , day:day, models/tableInfo.json::monday(for example monday) },
+     {...}]
+    */
     //send it!
-    tableView(tableData);
+    //tableView(tableData);
 }
