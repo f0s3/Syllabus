c52642df43b1db95b9fc31070c50616a37b977fd
f0s3 <power.aerozoll@gmail.com>
2018-04-17 23:32:24 +0300

    #33 v0.2. Back-end: TableController works as expected(for-loop bug fixed). Refactored code.

--- public/js/controllers/TableController.js
+++ public/js/controllers/TableController.js
@@ -5,128 +5,81 @@ function TableController(weekDays) {
             for (let weekdaysCount = 0;weekdaysCount < weekDays.length;weekdaysCount++) {
                 switch (weekDays[weekdaysCount]) {
                     case 1:
-                        //logic of tableView() - create an empty table.
+                        let mon = "monday";
                         $('#table-content')
-                            .append("<p id='monday-label'>Понеділок</p>")
-                            .append("<table id='monday-table' class='table-bordered table-responsive-sm'></table>"); //tableView()
-                        TableView("monday");
-                        //fill with data
-                        for (let i = response["monday"]["lessonId"][0];i <= response["monday"]["lessonId"].length;i++) {
-                            $("#table-body-monday").append("<tr id='monday-table-item-'></tr>");
-                            $("#monday-table-item-").attr("id", "monday-table-item-" + i);
-                            $("#monday-table-item-" + i)
-                                .append("<td id='monday-lessonId-'></td>")
-                                .append("<td id='monday-time-'></td>")
-                                .append("<td id='monday-cabinet-'></td>")
-                                .append("<td id='monday-subject-'></td>");
-                            $("#monday-lessonId-").attr("id", "monday-lessonId-" + i);
-                            $("#monday-lessonId-" + i).text(i);
-                            $("#monday-time-").attr("id", "monday-time-" + i);
-                            $("#monday-time-" + i).text(response["monday"]["time"][i - 1]);
-                            $("#monday-cabinet-").attr("id", "monday-cabinet-" + i);
-                            $("#monday-cabinet-" + i).text(response["monday"]["cabinet"][i - 1]);
-                            $("#monday-subject-").attr("id", "monday-subject-" + i);
-                            $("#monday-subject-" + i).text(response["monday"]["subject"][i - 1]);
+                            .append("<p id='"+ mon + "-label'>Понеділок</p>")
+                            .append("<table id='"+ mon + "-table' class='table-bordered table-responsive-sm'></table>"); //tableView()
+                        TableView(mon);
+                        for (let i = response[mon]["lessonId"][0];i <= response[mon]["lessonId"].length;i++) {
+                            fillWithData(mon, i);
+                            $("#" + mon + "-time-" + i).text(response[mon]["time"][i - 1]);
+                            $("#" + mon + "-cabinet-" + i).text(response[mon]["cabinet"][i - 1]);
+                            $("#" + mon + "-subject-" + i).text(response[mon]["subject"][i - 1]);
                         }
                         break;
                     case 2:
+                        let tues = "tuesday";
                         $('#table-content')
-                            .append("<p id='tuesday-label'>Вівторок</p>")
-                            .append("<table id='tuesday-table' class='table-bordered table-responsive-sm'></table>");
-                        TableView("tuesday");
-                        //fill with data
-                        for (let i = response["tuesday"]["lessonId"][0];i <= response["tuesday"]["lessonId"].length;i++) {
-                            $("#tuesday-table").append("<tr id='tuesday-table-item-'></tr>");
-                            $("#tuesday-table-item-").attr("id", "tuesday-table-item-" + i);
-                            $("#tuesday-table-item-" + i)
-                                .append("<td id='tuesday-lessonId-'></td>")
-                                .append("<td id='tuesday-time-'></td>")
-                                .append("<td id='tuesday-cabinet-'></td>")
-                                .append("<td id='tuesday-subject-'></td>");
-                            $("#tuesday-lessonId-").attr("id", "tuesday-lessonId-" + i);
-                            $("#tuesday-lessonId-" + i).text(i);
-                            $("#tuesday-time-").attr("id", "tuesday-time-" + i);
-                            $("#tuesday-time-" + i).text(response["tuesday"]["time"][i - 1]);
-                            $("#tuesday-cabinet-").attr("id", "tuesday-cabinet-" + i);
-                            $("#tuesday-cabinet-" + i).text(response["tuesday"]["cabinet"][i - 1]);
-                            $("#tuesday-subject-").attr("id", "tuesday-subject-" + i);
-                            $("#tuesday-subject-" + i).text(response["tuesday"]["subject"][i - 1]);
+                            .append("<p id='" + tues + "-label'>Вівторок</p>")
+                            .append("<table id='" + tues + "-table' class='table-bordered table-responsive-sm'></table>");
+                        TableView(tues);
+                        for (let i = response[tues]["lessonId"][0];i <= response[tues]["lessonId"].length;i++) {
+                            fillWithData(tues, i);
+                            $("#" + tues + "-time-" + i).text(response[tues]["time"][i - 1]);
+                            $("#" + tues + "-cabinet-" + i).text(response[tues]["cabinet"][i - 1]);
+                            $("#" + tues + "-subject-" + i).text(response[tues]["subject"][i - 1]);
                         }
                         break;
                     case 3:
+                        let wednes = "wednesday";
                         $('#table-content')
-                            .append("<p id='wednesday-label'>Середа</p>")
-                            .append("<table id='wednesday-table' class='table-bordered table-responsive-sm'></table>");
-
-                        TableView("wednesday");
-                        //fill with data
-                        for (let i = response["wednesday"]["lessonId"][0];i <= response["wednesday"]["lessonId"].length;i++) {
-                            $("#wednesday-table").append("<tr id='wednesday-table-item-'></tr>");
-                            $("#wednesday-table-item-").attr("id", "wednesday-table-item-" + i);
-                            $("#wednesday-table-item-" + i)
-                                .append("<td id='wednesday-lessonId-'></td>")
-                                .append("<td id='wednesday-time-'></td>")
-                                .append("<td id='wednesday-cabinet-'></td>")
-                                .append("<td id='wednesday-subject-'></td>");
-                            $("#wednesday-lessonId-").attr("id", "wednesday-lessonId-" + i);
-                            $("#wednesday-lessonId-" + i).text(i);
-                            $("#wednesday-time-").attr("id", "wednesday-time-" + i);
-                            $("#wednesday-time-" + i).text(response["wednesday"]["time"][i - 1]);
-                            $("#wednesday-cabinet-").attr("id", "wednesday-cabinet-" + i);
-                            $("#wednesday-cabinet-" + i).text(response["wednesday"]["cabinet"][i - 1]);
-                            $("#wednesday-subject-").attr("id", "wednesday-subject-" + i);
-                            $("#wednesday-subject-" + i).text(response["wednesday"]["subject"][i - 1]);
+                            .append("<p id='" + wednes + "-label'>Середа</p>")
+                            .append("<table id='" + wednes + "-table' class='table-bordered table-responsive-sm'></table>");
+                        TableView(wednes);
+                        for (let i = response[wednes]["lessonId"][0];i <= response[wednes]["lessonId"].length;i++) {
+                            fillWithData(wednes, i);
+                            $("#" + wednes + "-time-" + i).text(response[wednes]["time"][i - 1]);
+                            $("#" + wednes + "-cabinet-" + i).text(response[wednes]["cabinet"][i - 1]);
+                            $("#" + wednes + "-subject-" + i).text(response[wednes]["subject"][i - 1]);
                         }
                         break;
                     case 4:
+                        let thurs = "thursday";
                         $('#table-content')
-                            .append("<p id='thursday-label'>Четверг</p>")
-                            .append("<table id='thursday-table' class='table-bordered table-responsive-sm'></table>");
-                        TableView("thursday");
-                        //fill with data
-                        for (let i = response["thursday"]["lessonId"][0];i <= response["thursday"]["lessonId"].length;i++) {
-                            $("#thursday-table").append("<tr id='thursday-table-item-'></tr>");
-                            $("#thursday-table-item-").attr("id", "thursday-table-item-" + i);
-                            $("#thursday-table-item-" + i)
-                                .append("<td id='thursday-lessonId-'></td>")
-                                .append("<td id='thursday-time-'></td>")
-                                .append("<td id='thursday-cabinet-'></td>")
-                                .append("<td id='thursday-subject-'></td>");
-                            $("#thursday-lessonId-").attr("id", "thursday-lessonId-" + i);
-                            $("#thursday-lessonId-" + i).text(i);
-                            $("#thursday-time-").attr("id", "thursday-time-" + i);
-                            $("#thursday-time-" + i).text(response["thursday"]["time"][i - 1]);
-                            $("#thursday-cabinet-").attr("id", "thursday-cabinet-" + i);
-                            $("#thursday-cabinet-" + i).text(response["thursday"]["cabinet"][i - 1]);
-                            $("#thursday-subject-").attr("id", "thursday-subject-" + i);
-                            $("#thursday-subject-" + i).text(response["thursday"]["subject"][i - 1]);
+                            .append("<p id='" + thurs + "-label'>Четверг</p>")
+                            .append("<table id='" + thurs + "-table' class='table-bordered table-responsive-sm'></table>");
+                        TableView(thurs);
+                        for (let i = response[thurs]["lessonId"][0];i <= response[thurs]["lessonId"].length;i++) {
+                            fillWithData(thurs, i);
+                            $("#" + thurs + "-time-" + i).text(response[thurs]["time"][i - 1]);
+                            $("#" + thurs + "-cabinet-" + i).text(response[thurs]["cabinet"][i - 1]);
+                            $("#" + thurs + "-subject-" + i).text(response[thurs]["subject"][i - 1]);
                         }
                         break;
                     case 5:
+                        let fri = "friday";
                         $('#table-content')
-                            .append("<p id='friday-label'>П'ятниця</p>")
-                            .append("<table id='friday-table' class='table-bordered table-responsive-sm'></table>");
-                        TableView("friday");
-                        //fill with data
-                        for (let i = response["friday"]["lessonId"][0];i <= response["friday"]["lessonId"].length;i++) {
-                            $("#friday-table").append("<tr id='friday-table-item-'></tr>");
-                            $("#friday-table-item-").attr("id", "friday-table-item-" + i);
-                            $("#friday-table-item-" + i)
-                                .append("<td id='friday-lessonId-'></td>")
-                                .append("<td id='friday-time-'></td>")
-                                .append("<td id='friday-cabinet-'></td>")
-                                .append("<td id='friday-subject-'></td>");
-                            $("#friday-lessonId-").attr("id", "friday-lessonId-" + i);
-                            $("#friday-lessonId-" + i).text(i);
-                            $("#friday-time-").attr("id", "friday-time-" + i);
-                            $("#friday-time-" + i).text(response["friday"]["time"][i - 1]);
-                            $("#friday-cabinet-").attr("id", "friday-cabinet-" + i);
-                            $("#friday-cabinet-" + i).text(response["friday"]["cabinet"][i - 1]);
-                            $("#friday-subject-").attr("id", "friday-subject-" + i);
-                            $("#friday-subject-" + i).text(response["friday"]["subject"][i - 1]);
+                            .append("<p id='" + fri + "-label'>П'ятниця</p>")
+                            .append("<table id='" + fri + "-table' class='table-bordered table-responsive-sm'></table>");
+                        TableView(fri);
+                        for (let i = response[fri]["lessonId"][0];i <= response[fri]["lessonId"].length;i++) {
+                            fillWithData(fri, i);
+                            $("#" + fri + "-time-" + i).text(response[fri]["time"][i - 1]);
+                            $("#" + fri + "-cabinet-" + i).text(response[fri]["cabinet"][i - 1]);
+                            $("#" + fri + "-subject-" + i).text(response[fri]["subject"][i - 1]);
                         }
                         break;
                 }
             }
         });
+}
+function fillWithData(day, i) {
+    $("#table-body-" + day).append("<tr id='" + day + "-table-item-'></tr>");
+    $("#" + day + "-table-item-").attr("id", day + "-table-item-" + i);
+    $("#" + day + "-table-item-" + i)
+        .append("<td id='" + day + "-lessonId-"+ i + "'></td>")
+        .append("<td id='" + day + "-time-" + i + "'></td>")
+        .append("<td id='" + day + "-cabinet-" + i + "'></td>")
+        .append("<td id='" + day + "-subject-" + i + "'></td>");
+    $("#" + day + "-lessonId-" + i).text(i);
 }
