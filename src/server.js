4de895e455fff6bf50110342c3b00af94c830474
f0s3 <power.aerozoll@gmail.com>
2018-04-25 23:01:14 +0300

    #41 v0.4.(beta) Back-end: logging errors in the file.Hope that next few commits will be attempts to connect some db.

--- src/server.js
+++ src/server.js
@@ -5,12 +5,9 @@ let path = require('path');
 let app = express();
 let port = 4444;
 let directory = __dirname.substring(0, __dirname.length - 4);
+let accessLogStream = fs.createWriteStream(path.join(__dirname, 'access.log'), {flags: 'a'});
 
 app.use(express.static(directory + "/public/"));
-
-let accessLogStream = fs.createWriteStream(path.join(__dirname, 'access.log'), {flags: 'a'});
-app.use(morgan('combined', {stream: accessLogStream}));
-
-
+app.use(morgan('[:date:[web]] :remote-addr :method :url :status :response-time ms', {stream: accessLogStream}));
 app.get('/', (req, res) => res.sendFile(directory + "/public/" + "html/" + "index.html"));
 app.listen(port,() => console.log('app listening on port '+ port +'!'));
