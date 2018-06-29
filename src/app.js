392f2d938ab141b4d7b2e4b5e6ecf857017b6e42
f0s3 <power.aerozoll@gmail.com>
2018-06-29 13:30:03 +0300

    #53 Server is working flawlessly(I guess).

--- src/app.js
+++ src/app.js
@@ -11,48 +11,51 @@ app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({extended: false}))
 app.use(cors())
 app.get(`/`, (req, res) => {
-  
-/*
-  res.send({
-    message: `Hello user! You've chosen ${req.query.faculty} faculty and ${req.query.group} is your group and also ${req.query.subgroup} is your subgroup!`
-  });
-*/  
   let connection = mysql.createConnection({
     host:config.host_DB,
     user:config.user_DB,
-    password:config.password_DB,
-    db:`Syllabus_NNIIM`
+    password:config.password_DB
   })
-  connection.connect((err) => {
-    if (err) throw err;
-    console.log("Connected!");
-    let table = (req.query.subgroup === "") ? req.query.group : req.query.subgroup;
-    connection.query('SELECT monday FROM `' + table + '`;', (err, result) => {
-      if (err) throw err;
-      res.json(result);
-      console.log("db returned us this: ", res.json(result));
-    });
-  });
+
+  if (!req.query.weekdays) req.query.weekdays = '*';
+
+  if (!req.query.faculty && (!req.query.group)) {
+    res.send({
+      alert: 'Oops. I have found no faculty and group. Try to type that first and I will give you some info ;)'
+    })
+  } else if ((!req.query.faculty) && (req.query.group)) {
+    res.send({
+      alert: 'Would be better if you enter the faculty ;)'
+    })
+  } else if ((req.query.faculty) && (!req.query.group)) {
+    res.send({
+      alert: 'Type in your group and I will give you some nice info ;)'
+    })
+  } else if ((req.query.faculty) && (req.query.group)) {
+    connection.connect((err) => {
+      if (err) throw err
+
+      connection.query(`USE ${req.query.faculty};`, (err) => {
+        if (err) throw err
+      })
+
+      connection.query(`SELECT ${req.query.weekdays} FROM ${req.query.group};`, (err, result) => {
+        if (err) throw err
+        res.json(result)
+      })
+    })
+  }
 })
 
 app.listen(config.port || 4444, () => console.log('App is listening on port '+ config.port +'!'))
 
-/**
-* CREATE TABLE IF NOT EXISTS `711` (
-  `id` tinyint(5) unsigned NOT NULL AUTO_INCREMENT,
-  `monday` varchar(64) CHARACTER SET utf8 COLLATE utf8_unicode_ci NOT NULL DEFAULT '',
-  `tuesday` varchar(64) CHARACTER SET utf8 COLLATE utf8_unicode_ci NOT NULL DEFAULT '',
-  `wednesday` varchar(64) CHARACTER SET utf8 COLLATE utf8_unicode_ci NOT NULL DEFAULT '',
-  `thursday` varchar(64) CHARACTER SET utf8 COLLATE utf8_unicode_ci NOT NULL DEFAULT '',
-  `friday` varchar(64) CHARACTER SET utf8 COLLATE utf8_unicode_ci NOT NULL DEFAULT '',
-  PRIMARY KEY (`id`)
+/* exapmle of adding new group
+CREATE TABLE IF NOT EXISTS Д_711 (
+  id tinyint(3) NOT NULL AUTO_INCREMENT UNIQUE,
+  monday varchar(256) CHARACTER SET utf8 COLLATE utf8_unicode_ci NOT NULL DEFAULT '',
+  tuesday varchar(256) CHARACTER SET utf8 COLLATE utf8_unicode_ci NOT NULL DEFAULT '',
+  wednesday varchar(256) CHARACTER SET utf8 COLLATE utf8_unicode_ci NOT NULL DEFAULT '',
+  thursday varchar(256) CHARACTER SET utf8 COLLATE utf8_unicode_ci NOT NULL DEFAULT '',
+  friday varchar(256) CHARACTER SET utf8 COLLATE utf8_unicode_ci NOT NULL DEFAULT ''
 );
-INSERT INTO `711` (monday, tuesday, wednesday, thursday, friday, isSubgroup)
- VALUES (
- 'First lesson monday and the cabinet is _301_4',
- 'First lesson in tuesday and the cabinet is _21_5',
- 'First lesson in wednesday and the cabinet is _86_1',
- '',
- 'looks like thursday doesn\'t have anything btw cabinet is_253_2',
- 2);
-* */
+*/
